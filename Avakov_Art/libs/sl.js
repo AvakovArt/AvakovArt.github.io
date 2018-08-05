d9543f55e9238b787116f34a2b53040b262c9a1f
AvakovArt <41856604+AvakovArt@users.noreply.github.com>
2018-08-05 18:35:33 +0500

    Загрузил файлы
    Загрузка

---
+++ Avakov_Art/libs/sl.js
@@ -0,0 +1,11 @@
+ $('.testimonials').slick({
+ 	autoplay: true,
+ 	autoplaySpeed: 2000,
+ 	speed: 1000,
+ 	cssEase: 'ease-in',
+ 	centerMode: true,
+ 	centerPadding:'25px',
+ 	dots: true,
+ 	arrows: false,
+ 	pauseOnDotsHover: true
+ });
